f1417f87dcb3fba85335e971e575215de9755eb9
caldwell619 <caldwell8814@gmail.com>
2019-03-11 15:18:27 -0500

    initial commit

---
+++ src/main/java/com/burnnotice/burnnotice/BurnNoticeApplication.java
@@ -0,0 +1,13 @@
+package com.burnnotice.burnnotice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BurnNoticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BurnNoticeApplication.class, args);
+    }
+
+}
