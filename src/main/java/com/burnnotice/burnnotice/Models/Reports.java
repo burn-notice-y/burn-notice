0696f5f0cb0ef56343aff594a05781607ed354ef
Karla Nunez <karla@Karlas-MacBook-Pro.local>
2019-03-12 11:20:20 -0500

    finished reports model

--- src/main/java/com/burnnotice/burnnotice/Models/Reports.java
+++ src/main/java/com/burnnotice/burnnotice/Models/Reports.java
@@ -10,27 +10,127 @@ public class Reports {
     @Id @GeneratedValue
     private long id;
 
-    @Column
+    @Column(nullable=false)
     private String createDate;
 
-    @Column
+    @Column(nullable=false)
     private String exposedToChemicals;
 
-    @Column
+    @Column(nullable=false)
     private String timeDispatched;
 
-    @Column
+    @Column(nullable=false)
     private String timeArrived;
 
-    @Column
+    @Column(nullable=false)
     private String fireRetardantPresent;
 
-    @Column
+    @Column(nullable=false)
     private String primaryTeamActions;
 
-    @Column
+    @Column(nullable=false)
     private String secondaryTeamActions;
 
-    @Column
+    @Column(nullable=false)
     private String description;
+
+    // one creator of the report
+    @OneToOne
+    private User user;
+
+    //type of report being submitted
+//    @OneToOne
+//    private ReportType type;
+
+    public Reports() {
+
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getExposedToChemicals() {
+        return exposedToChemicals;
+    }
+
+    public void setExposedToChemicals(String exposedToChemicals) {
+        this.exposedToChemicals = exposedToChemicals;
+    }
+
+    public String getTimeDispatched() {
+        return timeDispatched;
+    }
+
+    public void setTimeDispatched(String timeDispatched) {
+        this.timeDispatched = timeDispatched;
+    }
+
+    public String getTimeArrived() {
+        return timeArrived;
+    }
+
+    public void setTimeArrived(String timeArrived) {
+        this.timeArrived = timeArrived;
+    }
+
+    public String getFireRetardantPresent() {
+        return fireRetardantPresent;
+    }
+
+    public void setFireRetardantPresent(String fireRetardantPresent) {
+        this.fireRetardantPresent = fireRetardantPresent;
+    }
+
+    public String getPrimaryTeamActions() {
+        return primaryTeamActions;
+    }
+
+    public void setPrimaryTeamActions(String primaryTeamActions) {
+        this.primaryTeamActions = primaryTeamActions;
+    }
+
+    public String getSecondaryTeamActions() {
+        return secondaryTeamActions;
+    }
+
+    public void setSecondaryTeamActions(String secondaryTeamActions) {
+        this.secondaryTeamActions = secondaryTeamActions;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+//    public ReportType getType() {
+//        return type;
+//    }
+//
+//    public void setType(ReportType type) {
+//        this.type = type;
+//    }
 }
