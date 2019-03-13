12478f5fd6cbfb5b19bf983f8143e57a06f77706
ShawnArnold1 <shawnthearnold@gmail.com>
2019-03-13 10:31:30 -0500

    COmmented some vacancy code while I work on it

--- src/main/java/com/burnnotice/burnnotice/Repositories/VacancyRepository.java
+++ src/main/java/com/burnnotice/burnnotice/Repositories/VacancyRepository.java
@@ -1,14 +1,14 @@
-package com.burnnotice.burnnotice.Repositories;
-
-
-import com.burnnotice.burnnotice.Models.Vacancy;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
-
-public interface VacancyRepository extends CrudRepository<Vacancy, Long>
-{
-//    Vacancy findOneVacancy(Vacancy vacancy);
+//package com.burnnotice.burnnotice.Repositories;
 //
-//    @Query(value = "select * from vacancies where role = ?1", nativeQuery = true)
-//    public Vacancy findByName(String title);
-}
+//
+//import com.burnnotice.burnnotice.Models.Vacancy;
+//import org.springframework.data.jpa.repository.Query;
+//import org.springframework.data.repository.CrudRepository;
+//
+//public interface VacancyRepository extends CrudRepository<Vacancy, Long>
+//{
+////    Vacancy findOneVacancy(Vacancy vacancy);
+////
+////    @Query(value = "select * from vacancies where role = ?1", nativeQuery = true)
+////    public Vacancy findByName(String title);
+//}
